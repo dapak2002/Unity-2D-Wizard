5c068dab831cd0775ab8534ba69cd6587c956e6c
dapak2002 <dapak@chapman.edu>
2021-05-03 15:43:52 -0700

    Added tilemap and objects

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+
+    public float runSpeed = 25f;
+    public bool hasJumpPotion = false;
+    public bool hasSpeedPotion = false;
+    public int potionModAmount = 0;
+
+    public AudioClip jumpClip;
+
+    private float potionTimeMax = 10f;
+    private float potionTimeCur = 0f;
+
+    bool jumpFlag = false;
+    bool jump = false;
+
+    float horizontalMove = 0f;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (jumpFlag)
+        {
+            animator.SetBool("IsJumping", true);
+            jumpFlag = false;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (animator.GetBool("IsJumping") == false)
+            {
+                AudioSource.PlayClipAtPoint(jumpClip, transform.position);
+                jump = true;
+                animator.SetBool("IsJumping", true);
+            }
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+        jump = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (hasJumpPotion && potionTimeCur < potionTimeMax)
+        {
+            controller.m_JumpForceMod = potionModAmount;
+            potionTimeCur += Time.fixedDeltaTime;
+        }
+        else
+        {
+            potionTimeCur = 0f;
+            controller.m_JumpForceMod = 0;
+            hasJumpPotion = false;
+        }
+
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if (jump)
+        {
+            jumpFlag = true;
+        }
+    }
+}
